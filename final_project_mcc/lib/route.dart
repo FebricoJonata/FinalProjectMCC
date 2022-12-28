2de2cd1de7c15d8d9dd75a0829e1b6c4270eb00b
Febrico Jonata <riko9647@gmail.com>
2022-12-29 00:09:13 +0700

    add login page UI

--- final_project_mcc/lib/route.dart
+++ final_project_mcc/lib/route.dart
@@ -1,4 +1,5 @@
 import 'package:final_project_mcc/HomePage.dart';
+import 'package:final_project_mcc/LoginPage.dart';
 import 'package:final_project_mcc/register.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,12 +11,14 @@ class RouterGenerator {
         return MaterialPageRoute(builder: (_) => HomePage());
       case '/register':
         return MaterialPageRoute(builder: (_) => Register());
+      case '/login':
+        return MaterialPageRoute(builder: (_) => LoginPage());
       default:
         return MaterialPageRoute(
             builder: (_) => Scaffold(
                   body: Center(
                       child: Text(
-                          'Tidak ada route yang ditentukan untuk ${settings.name}')),
+                          'Tidak ada route yang ditemukan untuk ${settings.name}')),
                 ));
     }
   }
