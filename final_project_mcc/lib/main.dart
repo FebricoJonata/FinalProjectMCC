9ac091a99a9cae4aca76ba77970964072271eb14
Febrico Jonata <riko9647@gmail.com>
2023-01-14 08:15:35 +0700

    update project

--- final_project_mcc/lib/main.dart
+++ final_project_mcc/lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:final_project_mcc/register.dart';
 import 'package:final_project_mcc/route.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'homepage.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
