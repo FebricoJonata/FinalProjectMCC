64273aa8480ef7c8f39f1c45325c3d7488399818
Febrico Jonata <riko9647@gmail.com>
2023-01-15 15:18:42 +0700

    resolve api problem

--- api-app/routes/users.js
+++ api-app/routes/users.js
@@ -3,18 +3,18 @@ var router = express.Router();
 var db = require("./connection.js");
 
 /* GET users listing. */
-// router.get("/read", function (req, res, next) {
-//   const sql = "SELECT * FROM users";
+router.get("/read", function (req, res, next) {
+  const sql = "SELECT * FROM users";
 
-//   db.query(sql, function (err, result) {
-//     if (err) throw err;
-//     console.log(result);
-//   });
-// });
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 
 router.post("/test", function (req, res, next) {
   const data = req.body;
-  const query = `INSERT INTO users (username, password) VALUES ('${data.username}', '${data.password})`;
+  const query = `INSERT INTO users (username, password) VALUES ('${data.username}', '${data.password}')`;
   console.log(req.body.username);
   db.query(query, (err, result) => {
     if (err) throw err;
