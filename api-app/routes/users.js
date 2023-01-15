6b81bf5002ccce88ccbad6f22b91872c9134f141
ClaudyaSalim <claudya.salim@gmail.com>
2023-01-15 22:57:55 +0700

    update back-end (2)

--- api-app/routes/users.js
+++ api-app/routes/users.js
@@ -6,7 +6,6 @@ const { route } = require("./index.js");
 /* GET users listing. */
 router.get("/read", function (req, res, next) {
   const sql = "SELECT * FROM users";
-
   db.query(sql, function (err, result) {
     if (err) throw err;
     console.log(result);
@@ -14,10 +13,9 @@ router.get("/read", function (req, res, next) {
   });
 });
 
-router.post("/login", function (req, res, next) {
+router.get("/login", function (req, res, next) {
   const data = req.body;
-  const query = `select * from users where email = "${data.email}"`;
-  console.log(req.body.email);
+  const query = `select * from users where email = "${data.email}" and password = "${data.password}"`;
   db.query(query, (err, result) => {
     if (err) throw err;
     res.send(result);
@@ -27,15 +25,10 @@ router.post("/login", function (req, res, next) {
 router.post("/register", function (req, res, next) {
   const data = req.body;
   const query = `insert into users (user_id, email, username, password, token) VALUES ('${data.user_id}', '${data.email}', '${data.username}', '${data.password}', '${data.token}')`;
-  console.log(req.body.username);
   db.query(query, (err, result) => {
     if (err) throw err;
     res.send(result);
   });
-});
-
-router.get("/trial", function (req, res, next){
-  console.log("Hello");
 });
 
 module.exports = router;
